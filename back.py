53fabef36d3529920c39ab74a10209d0fedc05c6
Alexandr Gomez <alexandrglm@users.noreply.github.com>
2025-03-19 06:41:06 +0100

    CORS fixes testing 2

--- back.py
+++ back.py
@@ -112,7 +112,16 @@ def render_about():
     return render_template('about/about.html')
 ##########################################################################
 # SocketIO shitties PRjoect 10
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(
+    app, cors_allowed_origins=[
+        'http://127.0.0.1:8080',
+        'https://127.0.0.1:8080',
+        'http://localhost:8080',
+        'https://localhost:8080',
+        'http://0.0.0.0:8080',
+        'https://0.0.0.0:8080'
+    ]
+)
 
 @socketio.on('exec_commander')
 def commander(data):
