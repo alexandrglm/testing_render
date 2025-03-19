45d29bb5f3a20f9fbe8a8dcd29a4457587b54a35
Alexandr Gomez <alexandrglm@users.noreply.github.com>
2025-03-19 06:14:20 +0100

    Debugging off.

--- back.py
+++ back.py
@@ -136,7 +136,7 @@ def commander(data):
 
 # run inits: Sockets - Flask main
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8080)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
