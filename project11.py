152e0c61913c393109a249dfe203a69174b5c0a0
Alexandr Gomez <alexandrglm@users.noreply.github.com>
2025-03-20 07:45:12 +0100

    Testing Project 11 VR Shaders

--- project11.py
+++ project11.py
@@ -7,9 +7,10 @@ from flask import Flask, Blueprint, jsonify
 
 project11 = Blueprint('project11', __name__)
 
+
 @project11.route('/api/data')
 def get_data():
     return jsonify({
         'model': 'monument_to_party_founding_dprk.glb', 
-        'position': [0, -1.5, -5] 
+        'position': [0, 0, 0] 
     })
