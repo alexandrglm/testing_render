4bfddc59f13b92e9e7f1ba3280bee29cb6f879b8
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-03-30 20:49:22 +0200

    Update back.py

--- back.py
+++ back.py
@@ -123,7 +123,14 @@ def cookies_notice():
         return Markup(render_template('_footer.html'))
 
     return dict(render_footer = render_footer)
+    
+############################################################################
+# favicon
+@app.route('/favicon.ico')
+def favicon():
 
+    return send_from_directory('static', 'favicon.ico', mimetype='img/vnd.microsoft.icon') 
+    
 ############################################################################
 # SOCKETIO CONFIGS
 socketio = SocketIO(
