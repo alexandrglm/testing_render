69c79596273d4e64572cb145509e812b0c794933
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-05-25 03:31:52 +0200

    Update back.py

--- back.py
+++ back.py
@@ -1,3 +1,12 @@
+# FILE:        ./server/main.py STABLE
+# BRANCH:        server-stable
+
+"""
+While testing new logics, comment all server modules / plugjins, excepting all those basics
+for Flask-Jinja-Routes.
+"""
+
+
 ############################################################################
 # Project:      Web Services demo back-end
 # Date:         2025, May. 17th
@@ -13,27 +22,27 @@ import threading
 from dotenv import load_dotenv
 import logging
 
-from server.main import main_server
+from server.main import main_server, routes_projects, routes_blogs
 from server.mainObjects import projects, static_pages, allowed_root_files
-from server.mainProjects import routes_projects
+# from server.mainProjects import routes_projects
 from server.mainCSSJinja import route_CSSJinja
 from server.mainStaticFiles import route_staticFiles
 from server.mainStaticRoutes import route_staticPages
 from server.mainRootAllowed import route_alloweds
 from server.mainCookies import route_Cookies
-from server.mainServices import service_manager
+# from server.mainServices import service_manager
 
-from serverModules.mail import start_server_email
-from serverModules.logs import start_server_logs
-from serverModules.keep import start_server_keep
-from serverModules.reacts import react_p14_build
-from serverModules.nukebots import start_nukebots
-from serverModules.telegram import telelog
-from serverModules.paneldos import start_admin
-from serverModules.panel_modules import start_module
+# from serverModules.mail import start_server_email
+# from serverModules.logs import start_server_logs
+# from serverModules.keep import start_server_keep
+# from serverModules.reacts import react_p14_build
+# from serverModules.nukebots import start_nukebots
+# from serverModules.telegram import telelog
+# from serverModules.paneldos import start_admin
+# from serverModules.panel_modules import start_module
 
-from project10 import commander
-from project12 import project12, socketio_opers
+# from project10 import commander
+# from project12 import project12, socketio_opers
 
 load_dotenv()
 
@@ -42,25 +51,25 @@ app = Flask(__name__)
 ############################################################################
 # 0. SERVER STARTUP
 
-#############
-# 0.0 Logs 
-start_admin(app)
-start_module(app)
-#############
-# 0.2 Logs 
-start_server_logs(app)
+# #############
+# # 0.0 Logs 
+# start_admin(app)
+# start_module(app)
+# #############
+# # 0.2 Logs 
+# start_server_logs(app)
 
-#############
-# 0.1 NukeBots
-start_nukebots(app)
+# #############
+# # 0.1 NukeBots
+# start_nukebots(app)
 
-#############
-# 0.4 Telegram
-telelog.init_app(app)
+# #############
+# # 0.4 Telegram
+# telelog.init_app(app)
 
-#############
-# 0.3 SMTP
-start_server_email(app)
+# #############
+# # 0.3 SMTP
+# start_server_email(app)
             
 #############
 # 1. MAIN ROUTES
@@ -69,6 +78,7 @@ main_server(app)
 #############
 # 2. PROJECTS ROUTES
 routes_projects(app)    
+routes_blogs(app)  
 #############
 # 3. CSS.jinja ROUTES
 route_CSSJinja(app)
@@ -101,19 +111,19 @@ socketio = SocketIO(
 ############################################################################
 # 9. REACT - PROJECT CALLBACKS
 
-#############
-# Project 14 (React)
-react_p14_build()
+# #############
+# # Project 14 (React)
+# react_p14_build()
 
 
 #############
 # Project 12: (py)MongoShell (via its own socketio def)
 os.environ['GEVENT_SUPPORT'] = 'True'
 
-#############
-# Project 10: WebShell (via Socketio)
-socketio.on_event('exec_commander', commander)
-socketio_opers(socketio)
+# #############
+# # Project 10: WebShell (via Socketio)
+# socketio.on_event('exec_commander', commander)
+# socketio_opers(socketio)
 
 
 ############################################################################
@@ -122,20 +132,20 @@ socketio_opers(socketio)
 if __name__ == '__main__':
 
 
-    # GETTIN SERVICES FROM mainServices
-    service_manager.register_services(app)
+    # # GETTIN SERVICES FROM mainServices
+    # service_manager.register_services(app)
 
 
 
-    # STARTING SPECIAL SERVICES
-    service_manager.start_service('telegram')
-    time.sleep(2)
+    # # STARTING SPECIAL SERVICES
+    # service_manager.start_service('telegram')
+    # time.sleep(2)
 
 
-    # STARTING REGULAR SERVICES -> Pending iterate from over services lists ...
-    for service_name in ['logs', 'mail', 'nukebots', 'keep']:
+    # # STARTING REGULAR SERVICES -> Pending iterate from over services lists ...
+    # for service_name in ['logs', 'mail', 'nukebots', 'keep']:
 
-        service_manager.start_service(service_name)
+    #     service_manager.start_service(service_name)
     
     
     
@@ -152,20 +162,20 @@ if __name__ == '__main__':
     
     
     
-    except KeyboardInterrupt:
+    # except KeyboardInterrupt:
         
-        print('\nCleanStopping serices ...\n')
+    #     print('\nCleanStopping serices ...\n')
 
-        telelog.stop()
+    #     telelog.stop()
 
-        for service_name in service_manager.services:
+    #     for service_name in service_manager.services:
 
-            service_manager.stop_service(service_name)
+    #         service_manager.stop_service(service_name)
     
     
     
-    except Exception as e:
-        app.logger.error(f'[DEBUG] -> Unexpected server error! : {str(e)}')
+    # except Exception as e:
+    #     app.logger.error(f'[DEBUG] -> Unexpected server error! : {str(e)}')
     
     
     finally:
