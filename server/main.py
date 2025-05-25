568c402fd657ca300bda5007504086b37dd7d121
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-05-25 03:29:25 +0200

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from server.mainObjects import blogs,projects,allowed_root_files,static_pages
+from server.mainObjects import projects,allowed_root_files,static_pages
 import os
 import importlib
 from flask import Flask, render_template, send_from_directory, request, jsonify
@@ -7,7 +7,7 @@ from markupsafe import Markup
 
 # FILE:        ./server/main.py STABLE
 # BRANCH:        server-stable
-
+"""
 # #########################
 #   MAIN ROUTES (server-stable branch)
 #   
@@ -15,21 +15,38 @@ from markupsafe import Markup
 #
 #       - projects -> combined_content (projects + blog)
 #       - 
-#       - Minded for New Objects' Value - Keys (Date, Type, Order)
-
-
+#       - Minding for New Objects' Value - Keys (Date, Type, Order)
+        - 
+        - Add rendering_checks (Objects -> Projects -> Mandarory fields)
+        - Add combined_content func ->Handler, Get and mix projects+blogs as intended
+        -
+        - Add main_blog_funcs (routes for self 'snapshots'). Might be exported to a new mainBlog.py logic snippet.
+        -
+"""
 def main_server(app):
     @app.route('/')
     def home():
         
         try:
-            combined_content = get_combined_content()
+            combined_content = main_combineds()
             return render_template('main.html', combined_content=combined_content)
         
         except Exception as e:
 
             print(f'[ERROR] (Server/main route) -> Main / failed! : {str(e)}')
             return render_template('404/index_404.html')
+        
+    
+    def main_combineds(): # pending -> Class plus constructors???
+
+        not_hidden = [p for p in projects if not p.get('hidden', False)]
+        
+        not_hidden.sort(key=lambda x: int(x.get('order', '999')))
+        
+        return not_hidden
+
+
+
 
 
 def routes_projects(app):
@@ -90,7 +107,6 @@ def routes_blogs(app):
     @app.route('/blog/<int:year>/<int:month>/<slug>/')
     def render_blog(year, month, slug):
         
-        
         # metadata.link + metadata.YYYY--metadata-MM
         blog = next((p for p in projects 
                     if p.get('type') == 'blog' 
@@ -112,3 +128,86 @@ def routes_blogs(app):
             
             print(f'\n[ERROR] (BLOGS) -> {template_path} error :\n   {str(e)}\n\n')
             return render_template('404/index_404.html')
+
+
+
+    def main_blog_snapshot(project):
+
+        metadata = project.get('metadata', {})
+        if metadata.get('snapshot'):
+            
+            date_splits = metadata.get('date', '').split('-')
+            
+            # Date -> Length valids
+            if len(date_splits) >= 2:
+
+                year, month = date_splits[0], date_splits[1]
+                
+                link = metadata.get('link', 'default')
+                
+                filename = f"blog/{year}/{month}/{link}/{metadata['snapshot']}"
+                return send_from_directory('static', filename)
+        
+        # add log '[WARN]') or smething
+        return send_from_directory('static', 'blog-default-cardboard.png')
+        
+
+
+
+# Pending -> Might be a class bor main/projects/blogs??????
+def rendering_checks():
+
+    for i, project in enumerate(projects):
+        """
+        MANDATORIES:
+            - Order     -> For JinjaRendering order    
+            - Type      -> 'project', 'blog', etc
+            - Size      -> Cardboards' rendering sizes (default, double, mino)
+            - Hidden    -> bool
+
+            If Type == 'project':
+                - id
+
+            If Type == 'Blog:
+                - Metadata -> Link  : 
+                - Metadata -> Date
+                - Metadata -> Title
+        """
+        # PENDING -> If Duplicated ORDER
+        
+        # FOR Projects+blogs SHARED MANDATORIES
+        if not project.get('order'):
+            print(f'\n[ERROR] (OBJECTS) -> "{i}" HAS NO "ORDER"\n\n')
+        
+        if not project.get('type'):
+            print(f'\n[ERROR] (OBJECTS) -> "{i}" HAS NO "TYPE"\n\n')
+        
+        if not project.get('size'):
+            print(f'\n[ERROR] (OBJECTS) -> "{i}" HAS NO "SIZE"\n\n')
+        
+        if 'hidden' not in project:
+            print(f'\n[ERROR] (OBJECTS) -> "{i}" HAS NO "HIDDEN"\n\n')
+
+        
+        # FOR Projetc MANDATORIES (ID's)
+        if project.get('type') == 'project' and not project.get('id'):
+            print(f'\n[ERROR] (OBJECTS) -> "{i}" is "PROJECT" but HAS NO "ID"\n\n')
+        
+        
+        # FOR Blog MANDATORIES (date, link)
+        if project.get('type') == 'blog':
+
+            metadata = project.get('metadata', {})
+            
+            if not metadata.get('link'):
+                print(f'\n[ERROR] (OBJECTS) -> "{i}" is "BLOG" but HAS NO "LINK"\n\n')
+            
+            if not metadata.get('date'):
+                print(f'\n[ERROR] (OBJECTS) -> "{i}" is "BLOG" but HAS NO "DATE"\n\n')
+        
+        # FOR ALL VALIDS
+        if not project.get('metadata', {}).get('title'):
+            print(f'\n[ERROR] (OBJECTS) -> "{i}" HAS NO "TITLE"\n\n')
+
+
+rendering_checks()
